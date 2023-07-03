4bc9ff4e6ae19c3ba619e29c7b84e38c934350b8
Francisco Santos <francisco.santos@ahrefs.com>
2023-07-03 20:22:24 +0100

    First commit

---
+++ _config.ts
@@ -0,0 +1,13 @@
+import lume from "lume/mod.ts";
+import date from "lume/plugins/date.ts";
+import pt from "npm:date-fns/locale/pt/index.js";
+
+const site = lume({
+  src: "./src",
+});
+
+site.use(date({
+  locales: { pt },
+}));
+
+export default site;
---
+++ src/posts/hello.md
@@ -0,0 +1,16 @@
+---
+layout: layouts/posts.njk
+url: /posts/hello.html
+title: Hello World
+description: Primeiro Post
+date: 2023-07-03
+tags:
+  - Tag1
+---
+
+
+# Ola!
+
+Ola, este e o meu primeiro post!
+
+Boa noite!
